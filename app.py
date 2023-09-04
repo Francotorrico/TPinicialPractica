c881bc16ae152f3012f48f43a3946177c56663cc
Franco <francotorrico1999@gmail.com>
2023-09-04 02:06:13 -0300

    practica

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request
 import pandas as pd
 from joblib import dump, load
 from jinja2.utils import escape
-#from itsdangerous import json as itsdangerous_json  # Cambia el nombre de la importación
-
 
 with open('diabetesCLasificacion.joblib','rb') as f:
     model = load(f)
